06762bac1276b50a697701a426c406660c0a86cb
PyroPM <sean.k.mapes@gmail.com>
2022-06-22 12:56:43 -0400

    nvm this is dumb

--- assets/scripts.js
+++ assets/scripts.js
@@ -16,10 +16,3 @@ function get_file(file) {
         }
     })
 }
-
-function resize_dropdowns() {
-    let dropdowns = document.getElementsByClassName("dropdown");
-    for (var i = 0; i < dropdowns.length; i++) {
-        dropdowns[i].children[1].offsetWidth = dropdowns[i].offsetWidth;
-    }
-}
