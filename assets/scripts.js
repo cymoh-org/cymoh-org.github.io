457b3731abd559b059937c94ca491870ce866530
PyroPM <sean.k.mapes@gmail.com>
2022-06-24 09:07:54 -0400

    what

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,6 +1,7 @@
 
 function init(file) {
     let markdown_text = get_file(file);
+    console.log(markdown_text);
     document.getElementById("body").innerHTML += marked.parse(markdown_text);
 }
 
@@ -8,7 +9,8 @@ function get_file(file) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onload = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            return(xmlhttp.responseText);
+            console.log(xmlhttp.responseText);
+            return xmlhttp.responseText;
         }
     }
     xmlhttp.open("GET", file, true);
